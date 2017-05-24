4b060e408f896daa097a8480a2ff9faaafe3b08d
tchambil <antony_epis@hotmail.com>
2017-05-23 23:44:31 -0500

    add project backend for app mobile

---
+++ WebHelpPeru/Controllers/IndexController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebHelpPeru.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
